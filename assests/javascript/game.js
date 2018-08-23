de347526f95fcbcf3660fa263a9a1027976b4a93
SebastianOrtega <sortega@moviltrack.com>
2018-08-23 15:49:00 -0500

    80% jalando

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     console.log("ready!");
-    var breeds = ["Akita", "Husky", "Alaskan", "Pit Bull", "Terrier", "Pug", "Bull Dog", "Chihuahua", "Dalmata", "Retriever", "Dachshund"];
-
+    var breeds = ["akita", "husky", "alaskan", "pit bull", "terrier", "pug", "bull dog", "chihuahua", "dalmata", "retriever", "dachshund"];
+    var usedLetters = [];
+    var badCounter = 90;
+    var goodCount = 0;
+    var keyboardDisabled = false;
 
 
 
@@ -9,6 +12,17 @@ $(document).ready(function() {
         var word;
         word = breeds[Math.floor((Math.random() * breeds.length))];
         return word;
+    }
+
+    function badLetter(badletter) {
+
+        console.log("Letra mala: " + badletter);
+        var letter = $("<div>");
+        $(letter).addClass("usedletter");
+        //$(letter).attr("data-letter", palabra[i]);
+        $(letter).text(badletter);
+        $(".usedletters").append(letter);
+
     }
 
 
@@ -22,12 +36,57 @@ $(document).ready(function() {
     for (var i = 0; i < palabra.length; i++) {
         console.log("Letra: " + palabra[i]);
         var letter = $("<div>");
-        $(letter).addClass("letter");
-        //$(letter).attr("data-letter", palabra[i]);
-        $(letter).text("___");
+        $(letter).addClass("usedletter no-word letter-" + palabra[i]);
+        $(letter).text(palabra[i]);
         $(".goodletter").append(letter);
 
     }
+
+    // Reload button (Play Again)
+    $(".btn").on("click", function() {
+        console.log("Reload");
+        location.reload();
+    });
+
+    // on Keydown
+
+    document.onkeyup = function(event) {
+        var keyDown = event.key;
+        console.log(keyDown);
+        if (!keyboardDisabled) {
+
+            if (palabra.indexOf(keyDown) !== -1) {
+
+                $(".letter-" + keyDown).addClass("good");
+                console.log(keyDown + "  Es buena");
+                var repetition = 0;
+                for (var n = 0; n < palabra.length; n++) {
+                    if (keyDown === palabra[n])
+                        repetition++;
+                }
+                console.log("Letras repetidas: " + repetition)
+
+
+            } else {
+                console.log(keyDown + "  Es mala");
+                var numeroImagen = ++badCounter;
+                if (badCounter === 100)
+                    keyboardDisabled = true;
+                var stringImagen = "assests/images/" + numeroImagen + ".jpg";
+                console.log("Numeor de imagen: " + stringImagen);
+
+
+                $("#imagen").attr("src", stringImagen);
+
+                badLetter(keyDown);
+            }
+        }
+
+    }
+
+
+
+
 
 
 
